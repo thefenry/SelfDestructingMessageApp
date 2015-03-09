bc5a8a9ac7d7192dbdfb0e6f777b505ca1f37f26
Ivan Sued <thefenry@gmail.com>
2015-03-09 14:28:07 -0700

    clean upspace

--- app/src/main/java/ivansued/com/selfdestructingmessageapp/MainActivity.java
+++ app/src/main/java/ivansued/com/selfdestructingmessageapp/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
