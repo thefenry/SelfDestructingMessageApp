80b03702400f6243b6b23b9238d07f19a29c75b1
Ivan Sued <thefenry@gmail.com>
2015-03-12 10:13:09 -0700

    Modify project to allow for tabbed navigation instead fo swipe. Also refactored string resources to use the appropriate names.

---
+++ app/src/main/java/ivansued/com/selfdestructingmessageapp/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package ivansued.com.selfdestructingmessageapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+/**
+ * Created by isued on 3/12/2015.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    protected Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return MainActivity.PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+        }
+        return null;
+    }
+}
